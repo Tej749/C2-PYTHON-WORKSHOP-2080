bbdd52556883d2a66c26101ef6f2298b6a61c35b
Tej749 <jargha261975@gmail.com>
2024-08-29 00:23:35 +0545

    upload datatype

---
+++ data_type.py
@@ -0,0 +1,37 @@
+'''
+Data Type
+Text type  : str
+numberic : int, float, complex
+sequential : list, tuple, set
+mapping : dic (key & value vair)
+boolean : bool (true & false)
+'''
+# String : it is an aray of character which is enclosed in comma (single, double)
+# name = "sumit"
+# name = "Krishna"
+# name = 'sudeep'
+# print(name)
+
+# college = "Neapalung Campus of Management & Technology"
+# splt = college.split()
+# print(splt)
+#
+# # Numeric
+# a = 56
+# print(type(a))
+#
+# sets = {'sumit', 'krishna', 'sumit', 'sudeep', 'sudeep', 'sudeep'}
+# print(sets)
+
+# Dictionary : It is a collecti0b of elements is stored in key - value pairs....which is enclose in curly bracket
+
+student = {
+    'id' : 101,
+    "name" : "Ayush",
+    'dob' : "07/14/2062",
+    'add' : "Nepalgunj",
+    'mob' : 9829645471
+}
+# print(student)
+print(student['name'])
+print(student['mob'])
